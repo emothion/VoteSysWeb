e6afe6bf0eb2df79cd5838dbe30a32af0c4c66e1
emothion <emothion@outlook.com>
2017-05-01 10:17:35 +0800

    新建联合查询服务接口

---
+++ src/com/votesys/service/query/inter/IQueryConjunctiveSV.java
@@ -0,0 +1,9 @@
+package com.votesys.service.query.inter;
+
+/**
+ * @ClassName com.votesys.service.query.inter.IQueryConjunctiveSV.java
+ * @Description 联合查询服务接口
+ */
+public interface IQueryConjunctiveSV {
+
+}
